99bdfcd12b4c688fbbcb630fe340c45a47361e90
Tobias Gilgenreiner <64074325+TobiasGilgenreiner@users.noreply.github.com>
2022-03-02 10:24:47 +0100

    Implemented Basic Structure

--- SSH_File_Reader/SSH_File_Reader/Program.cs
+++ SSH_File_Reader/SSH_File_Reader/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Renci.SshNet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -8,7 +10,56 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter IP-Addr:");
+            Console.Write("Enter SSH-Address: ");
+            string? AddressString = Console.ReadLine();
+            Console.Write("Enter Username: ");
+            string? UserNameString = Console.ReadLine();
+            Console.Write("Enter Password: ");
+
+            string PasswordString = string.Empty;
+            ConsoleKey key;
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+                if(key == ConsoleKey.Backspace && PasswordString.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    PasswordString = PasswordString.Remove(PasswordString.Length - 1);
+                }
+                else if(!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write('*');
+                    PasswordString+=keyInfo.KeyChar;
+                }
+            }while(key != ConsoleKey.Enter);
+
+
+            IPAddress? Address;
+            
+            if (!IPAddress.TryParse(AddressString, out Address))
+                throw new Exception("Invalid IPAddr");
+            if (Address == null)
+                throw new Exception("Invalid IPAddr");
+            if (UserNameString == null)
+                throw new Exception("Invalid Username");
+
+            using(var client = new SshClient(Address.ToString(), UserNameString, PasswordString))
+            {
+                try
+                {
+                    client.Connect();
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception("Connection Failed!\n" + ex.Message);
+                }
+                if (!client.IsConnected)
+                    throw new Exception("Couldn't connect to Target");
+
+                client.RunCommand("");
+                client.Disconnect();
+            }
         }
     }
 }
